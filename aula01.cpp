703f577844fcd1c94bd8bd958bf5ff434a120763
Nicole Silvestrini <nsgarrio@gmail.com>
2023-08-10 18:30:29 -0300

    Entrada e saída de dados

---
+++ aula01.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    //Programa 1 e 2
+    // cout<<"Olá. Bom dia! \n";
+    // cout<<"Bom dia para estudar ";
+    // cout<<"Programacao ";
+    // cout<<"em C/C++ !"<<endl;
+    
+    
+    //Programa 3
+    //int x;
+    //cout <<" Digite um número inteiro: ";
+    //cin >> x;
+    //cout << "\n\n --- Valor digitado: "<<x<<endl;
+    
+    //Programa 4
+    //int numero1, numero2, soma;
+    //cout<<"Digite um número inteiro: ";
+    //cin>>numero1;
+    //cout<<"Digite OUTRO número inteiro: ";
+    //cin>>numero2;
+    //soma = numero1+numero2;
+    //cout<<"\n\n --- Resultado: ";
+    //cout << numero1 << " + " << numero2 << " = " << soma;
+    
+    //Programa 5
+    /*int base, altura, area;
+    cout<<"Digite a medida da base do quadrilatero: ";
+    cin>>base;
+    cout<<"Digite a medida da altura do quadrilatero: ";
+    cin>>altura;
+    area = base*altura;
+    cout<<"base x altura = area do quadrilatero"<<endl;
+    cout<<base<<" x "<<altura<<" = "<<area;*/
+    
+    //Programa 6 >> Precisa de if
+    /*int dividendo, divisor, quociente, resto;
+    cout<<"Digite o dividendo: ";
+    cin>>dividendo;
+    cout<<"Digite o divisor: ";
+    cin>>divisor;*/
+    
+    //Programa 7
+    /*float nota1, nota2, media;
+    cout<<"Digite a nota da primeira prova: ";
+    cin>>nota1;
+    cout<<"Digite a nota da segunda prova: ";
+    cin>>nota2;
+    media = (nota1*4+nota2*6)/10;
+    cout<<"Nota 1 = "<<nota1<<"\nNota 2 = "<<nota2<<"\nMedia = "<<media;*/
+    
+    //Programa 8 >> Formula: c=5/9*(f-32)
+    /*float temperatura_fahrenheit, temperatura_celsius;
+    cout<<"Digite a temperatura em Fahrenheit: ";
+    cin>>temperatura_fahrenheit;
+    temperatura_celsius = 5./9*(temperatura_fahrenheit-32);
+    cout<<temperatura_fahrenheit<<"F é igual a "<<temperatura_celsius<<"ºC";*/
+    
+    //Programa 9 >> Precisa de if
+    
+    //Programa 10
+    float x;
+    int y, resultado;
+    cout<<"Digite o valor de X: ";
+    cin>>x;
+    cout<<"Digite o valor de Y: ";
+    cin>>y;
+    resultado = pow(x,y);
+    cout<<x<<"^"<<y<<" = "<<resultado;
+    
+    return 0;
+}
