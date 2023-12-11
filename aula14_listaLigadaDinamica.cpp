d71b2f59dc9366ef01f3631b8cba46a2b913c374
Nicole Silvestrini <nsgarrio@gmail.com>
2023-12-11 08:16:15 -0300

    Lista ligada

--- aula14_listaLigadaDinamica.cpp
+++ aula14_listaLigadaDinamica.cpp
@@ -1,249 +1,203 @@
 #include <iostream>
+#include <cctype>
 #include <cstdlib> // Para utilizar a função malloc, calloc ou realloc.
 using namespace std;
 
 // Lista ligada/ Dinâmica
 // Exercício 01
 typedef int elemento;
-typedef struct no{
+typedef struct no
+{
     elemento info;
-    no*link;
-}no;
+    no *link;
+} no;
 
-no*init(){
+no *init()
+{
     return NULL;
 }
 
-no*novo_no(){
-    no*novo;
-    novo=(no*)malloc(sizeof(no));
-    if (novo==NULL){
-        cout<<"Erro de alocação de memória!";
+no *novo_no()
+{
+    no *novo;
+    novo = (no *)malloc(sizeof(no));
+    if (novo == NULL)
+    {
+        cout << "Erro de alocação de memória!";
         exit(1);
     }
     return novo;
 }
 
-void imprime(no*pri){
-    while (pri!=NULL)
+void imprime(no *pri)
+{
+    cout << endl;
+    while (pri != NULL)
     {
-        cout<<pri->info<<"  ";
-        pri=pri->link;
+        cout << pri->info << "  ";
+        pri = pri->link;
     }
-}
-
-void inserir_final(no**pri, elemento a){
-    no*novo, *p;
-    novo=novo_no();
-    novo -> info=a;
-    novo->link=NULL;
-    p=*pri;
-    while (p!=NULL && p->link!=NULL)
-        p=p->link;
-    if (p==NULL)
-        *pri=novo;
-    else p->link=novo;
+    cout << endl;
 }
 
 // void inserir_inicio(no**pri, elemento a){
-//     no*novo;
-//     novo=novo_no();
+//     no*novo; novo=novo_no();
 //     novo->info=a;
-//     novo->link = *pri;
+//     novo->link=*pri;
 //     *pri=novo;
-    
-// }
-
-// void inserir_ordenado(no**pri, elemento a){
-//     no*novo, *p, *anterior;
-//     novo=novo_no();
-//     anterior=NULL;
-//     novo->info=a;
-//     p=*pri;
-//     while (p!=NULL && a > p->info)    {
-//         anterior=p;
-//         p=p->link;
-//     }
-//     if (p==*pri) *pri=novo;
-//     else anterior->link=novo;
-//     novo->link=p;
-    
-// }
-
-int main(){
-    no *pri, *Pmaior=pri, *Pmenor=pri;
-    pri=init();
-    elemento valor;
-    int n, i;
-    cout<<"Digite a quantidade de valores que serão inseridos:  "; cin>>n;
-    for (i=0; i<n; i++){
-        cout<<"Valor "<<i+1<< ": "; cin>> valor;
-        inserir_final(&pri, valor);
-        // inserir_ordenado(&pri, valor);
-        // inserir_inicio(&pri, valor);
-    }
-    no*p=pri;
-    while (p!=NULL)
-    {
-        if (p->info > Pmaior->info)
-            Pmaior = p;
-        else if (p->info < Pmenor->info)
-            Pmenor = p;
-        p = p->link;
-    }
-    cout<<"Valores: "; imprime(pri);
-    cout<<"\nMaior: "<<Pmaior->info<<endl;
-    cout<<"Menor: "<<Pmenor->info<<endl;
-}
-
-
-
-
-// Exercício 02 - CORRIGIR
-// typedef float elemento;
-// typedef struct no{
-//     elemento info;
-//     no*link;
-// }no;
-
-// void imprimir(no*pri){
-//     while (pri!=NULL)
-//     {
-//         cout<<pri->info<<"  ";
-//         pri=pri->link;
-//     }
-// }
-
-// no*novo_no(){
-//     no*novo;
-//     novo=(no*)malloc(sizeof(no));
-//     if(novo==NULL){
-//         cout<<"Erro de alocação de memória!";
-//         exit(1);
-//     }
-//     return novo;
 // }
 
 // void inserir_final(no**pri, elemento a){
 //     no*novo, *p;
-//     p=*pri;
 //     novo=novo_no();
-//     novo->info=a;
+//     novo -> info=a;
 //     novo->link=NULL;
+//     p=*pri;
 //     while (p!=NULL && p->link!=NULL)
 //         p=p->link;
 //     if (p==NULL)
 //         *pri=novo;
 //     else p->link=novo;
-    
 // }
 
-// void criar_vetor(no*pri, int n, elemento a){
-//     cout<<"Quantidade de notas que serão inseridas: "; cin>>n;
-//     for (int i=0; i<n; i++){
-//         cout<<"Nota "<<i+1<<": "; cin>>a;
-//         inserir_final(&pri, a);
-//     }
-// }
+void inserir_ordenado(no **pri, elemento a)
+{
+    no *novo, *p, *anterior;
+    novo = novo_no();
+    novo->info = a;
+    p = *pri;
+    anterior = NULL;
+    while (p != NULL && a > p->info)
+    {
+        anterior = p;
+        p = p->link;
+    }
+    if (p == *pri)
+        *pri = novo;
+    else
+        anterior->link = novo;
+    novo->link = p;
+}
 
-// int mais_repete(no*pri, int n){
-//     int rep = 0, contar;
-//     elemento maior;
-//     no *p, *q;
-//     if(pri==NULL)return -1;
-//     p=pri;
-//     while (p->link!=NULL)
+// Ecercício 01 - Dada uma lista ligada. Construir uma função que conta o número de nós dessa lista. O número de nós encontrado deverá retornar pelo comando return.
+int conta_nos(no *pri)
+{
+    no *p = pri;
+    int cont = 1;
+    if (pri == NULL)
+        return 0;
+    while (p->link != NULL)
+    {
+        p = p->link;
+        cont++;
+    }
+    return cont;
+}
+
+// Exercício 02 e 03
+// 02 -  Dada uma lista ligada contendo números inteiros ordenados. Construir uma função que verifica se há algum número repetido na lista ou não. 
+// 03 - Dada uma lista ligada contendo números inteiros desordenados. Construir uma função que verifica se há algum número repetido na lista ou não.
+// Não usar estruturas auxiliares para a solução, tais como, vetor ou outra lista ligada
+// Caso exista alguma repetição retornar 1 senão retornar 0.
+int repetidos(no *pri)
+{
+    no *p = pri;
+    int rep = 0;
+    while (pri != NULL)
+    {
+        p = pri->link;
+        while (p != NULL)
+        {
+            if (pri->info == p->info)
+            {
+                rep = 1;
+                break;
+            }
+            p = p->link;
+        }
+        pri = pri->link;
+    }
+    return rep;
+}
+
+// Exercício 04 - Dada uma lista ligada contendo números inteiros. Construir uma função que verifica se a lista está ordenada ou não. Se ordenada retornar 1 senão retornar 0. 
+//  Não usar estruturas auxiliares para a solução, tais como, vetor ou outra lista ligada.
+int verifica_ordenacao(no *pri)
+{
+    no *p = pri, *anterior = p;
+    int ordenada = 1; // É ordenada
+    while (p != NULL)
+    {
+        if (anterior->info > p->info)
+        {
+            ordenada = 0; // Não é ordenada
+            break;
+        }
+        anterior=p;
+        p=p->link;
+    }
+    return ordenada;
+}
+
+//Exercício prova - Dada uma lista ligada contendo caracteres quaisquer, construir uma função que conta as vogais 'a' e 'A'.
+// int contar_vogaisA(no *pri)
 //     {
-//         q=p->link; contar=1;
-//         while (q!=NULL)
+//         int cont = 0;
+//         while (pri != NULL)
 //         {
-//             if (p->info==q->info)contar++;
-//             q=q->link;
+//             char c = pri->info;
+//             // Converte o caractere para maiúscula antes de comparar
+//             char upperC = toupper(c);       
+//             // Verifica se é a vogal 'A'
+//             if (upperC == 'A')
+//                 cont++;
+//             pri = pri->link;
 //         }
-//         if (contar>rep){
-//             rep=contar;
-//             maior=p->info;
-//         }
-//         p=p->link;
+//         return cont;
 //     }
-//     return maior, contar;
-// }
 
-// int main(){
-//     no*pri;
-//     int n;
-//     elemento a;
-//     criar_vetor(pri, n, a);
-//     imprimir(pri);
-//     cout<<mais_repete(pri, n);
-// }
+// Exercício prova - Dada uma lista ligada contendo números inteiros quaisquer, construir uma função que insere uma cópia do último nó logo após ele.
+// Testar se está vazia e imprimir mensagem
+void duplica_ultimo(no*pri){
+    no*anterior=NULL, *novo;
+    novo=novo_no();
+    if(pri==NULL){
+        cout<<"Lista vazia!";
+        return;
+    }
+    while (pri->link!=NULL)
+    {
+        anterior=pri;
+        pri=pri->link;
+    }
+    novo->info=pri->info;
+    pri->link=novo;
+    novo->link=NULL;
+}
 
+int main()
+{
+    no *pri;
+    pri = init();
+    elemento num;
+    // int n;
+    // cout << "Quantidade de elementos: ";
+    // cin >> n;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Elemento: ";
+        cin >> num;
+        // inserir_inicio(&pri, num);
+        // inserir_final(&pri, num);
+        inserir_ordenado(&pri, num);
+    }
+    imprime(pri);
+    cout << "Quantidade de nos: " << conta_nos(pri);
+    cout << endl<< "Repetidos (1=sim e 0=nao): " << repetidos(pri);
+    cout<<endl<<"Ordenada (1=sim e 0=nao): "<< verifica_ordenacao(pri);
+    // cout<<endl<<"Quantidade de 'a' e 'A': "<< contar_vogaisA(pri);
 
-
-//Exercício 03
-
-
-
-// Exercício 04
-// struct Aluno {
-// int ra;
-// char nome[31];
-// float nota;
-// };
-
-// void ler_alunos(int *N, struct Aluno **alunos){
-// cout<<"Digite a quantidade de alunos: ";
-// cin>>*N;
-// // usando malloc ----> *alunos = (struct Aluno*)malloc(*N * sizeof(struct Aluno));
-// alunos = (struct Aluno*)calloc(*N, sizeof(struct Aluno));
-// for(int i=0; i<*N; i++) {
-// cout<<"Digite o RA do aluno: ";
-// cin>>(alunos)[i].ra;
-// cin.ignore();
-// cout<<"Digite o nome do aluno: ";
-// cin.getline((alunos)[i].nome, 31); //nao pode usar -> porque é um vetor não simples, não tem apenas uma linha de inf no strcut
-// cout<<"Digite a nota do aluno: ";
-// cin>>(alunos)[i].nota;}
-// }
-
-// float calcularMedia(int N, struct Aluno *alunos){
-// float soma=0;
-// for(int i=0; i<N; i++){
-// soma+= alunos[i].nota;}
-// return soma/N;
-// }
-
-// void imprimirAlunos(int N, struct Aluno *alunos){
-// cout<<"\nRA\tNome\tNota\n";
-// for (int i=0; i<N; i++){
-// cout<<alunos[i].ra <<" "<<alunos[i].nome<< " " <<alunos[i].nota<<endl;}
-// }
-
-// int main(){
-// int N;
-// struct Aluno *alunos;
-// float media;
-
-// ler_alunos(&N, &alunos);
-// media=calcularMedia(N, alunos);
-
-// cout<<"\nDados dos alunos:\n";
-// imprimirAlunos(N, alunos);
-
-// cout<<"\nMédia das notas: "<<media<<endl;
-// //sempre usar o free para tirar o espaço de memoria aberto pelo malloc.
-// free(alunos);
-// return 0;
-// }
-
-
-
-//Exercício 05
-// typedef produto prod;
-// typedef struct {
-//     int codigo;
-//     char nome[31];
-//     float preco;
-//     int quantidade;
-// }produto;
-
+    duplica_ultimo(pri);
+    imprime(pri);
+    return 0;
+}
